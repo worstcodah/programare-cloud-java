e7438a9be5ac30bd389a49ce4f49f42b82e58137
Rafael <rafael_9122@yahoo.com>
2022-04-07 18:57:35 +0300

    Final commit (homework 6).

--- lab-6-api-gateway/service2/src/main/java/com/example/service2/Product.java
+++ lab-6-api-gateway/service2/src/main/java/com/example/service2/Product.java
@@ -1,28 +1,18 @@
 package com.example.service2;
 
-import com.example.service2.base.AbstractEntity;
-import com.example.service2.util.DateProcessor;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Product extends AbstractEntity {
+public class Product  {
     interface BasicValidation {
     }
 
@@ -44,9 +34,7 @@ public class Product extends AbstractEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         var product = (Product) o;
-        if (!Objects.equals(id, product.id)) return false;
-        return Objects.equals(name, product.name) &&
-                Objects.equals(quantity, product.quantity);
+        return Objects.equals(name, product.name);
     }
 
     @Override
